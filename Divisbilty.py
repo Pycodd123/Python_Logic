21781b84427f4366a7098b3550d0f115c4afaf95
Parth Amradkar <parthamradkar@Purus-MacBook-Pro.local>
2021-11-15 10:20:17 -0800

    first commit

---
+++ Divisbilty.py
@@ -0,0 +1,8 @@
+def fun(num1):
+    answer = num1%2
+    answer1 = num1%3
+    if answer   == 0 and answer1 == 0:
+        print(num1, "Is Divisable By Two And Three")
+    else:
+        print(num1, "Is Not Divisable By Two And Three")    
+fun(8)        
